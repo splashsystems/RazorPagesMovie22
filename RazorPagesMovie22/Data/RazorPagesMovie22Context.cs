8665f77a1102e79b5f5d008973ebb250dc92089a
Scot <bscot.hagen@gmail.com>
2021-08-03 11:53:17 -0700

    Add project files.

---
+++ RazorPagesMovie22/Data/RazorPagesMovie22Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie22.Models;
+
+namespace RazorPagesMovie22.Data
+{
+    public class RazorPagesMovie22Context : DbContext
+    {
+        public RazorPagesMovie22Context (DbContextOptions<RazorPagesMovie22Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie22.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie22/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie22.Models;
+
+namespace RazorPagesMovie22.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie22.Data.RazorPagesMovie22Context _context;
+
+        public IndexModel(RazorPagesMovie22.Data.RazorPagesMovie22Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
